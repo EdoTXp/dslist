92af8bbc51e4574f7216fd59aa2df2499011c57b
Edoardo Fabrizio De Iovanna <edoardofabriziodeiovanna@hotmail.com>
2025-01-21 21:38:24 -0300

    SQL query, projection, game search by list

--- src/main/java/com/deiovannagroup/dslist/dto/GameMinDTO.java
+++ src/main/java/com/deiovannagroup/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.deiovannagroup.dslist.dto;
 
 import com.deiovannagroup.dslist.entities.Game;
+import com.deiovannagroup.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
     private Long id;
@@ -27,6 +28,14 @@ public class GameMinDTO {
         year = entity.getYear();
         imgUrl = entity.getImgUrl();
         shortDescription = entity.getShortDescription();
+    }
+
+    public GameMinDTO(GameMinProjection projection) {
+        id = projection.getId();
+        title = projection.getTitle();
+        year = projection.getYear();
+        imgUrl = projection.getImgUrl();
+        shortDescription = projection.getShortDescription();
     }
 
     public Long getId() {
